b9c9e81af739a3bc86e5efd5244c73fb362ad045
yash-op7 <yashm3375@gmail.com>
2024-10-07 20:49:51 +0530

    frontend: implmented logout;

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
 import { useAuthContext } from '../context/useAuthContext'
 import { Button } from "@/components/ui/button";
+import axios from '../api/axios';
 
 
 const Navbar = () => {
-    const {isLoggedIn} = useAuthContext();
+    const {isLoggedIn, setIsLoggedIn} = useAuthContext();
+    const handleLogout = async() => {
+        try {
+            await axios.post('/auth/logout', {}, {withCredentials:true});
+            setIsLoggedIn(false);
+            console.log('log out successful.')
+        } catch (error) {
+            if (error.response) {
+                console.error('Error response:', error.response.data);
+                alert(`Error: ${error.response.data.message || 'Logout failed'}`);
+            } else if (error.request) {
+                console.error('Error request:', error.request);
+                alert('Error: No response from server');
+            } else {
+                console.error('Error:', error.message);
+                alert(`Error: ${error.message}`);
+            }
+
+        }
+    }
   return (
     <div className='bg-black h-1/5 fixed top-0 flex flex-row justify-around items-center w-screen'>
         <h1 className='text-3xl text-white'>Task CRUD App</h1>
-        {isLoggedIn ? <Button>Logout</Button>:<Button>Login</Button>}
+        {isLoggedIn ? <Button onClick={handleLogout}>Logout</Button>:<Button>Login</Button>}
     </div>
   )
 }
